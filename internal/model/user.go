b32f8642baeaccb80b47bf750e062a79f779a6c7
AswinPuthukaatil <aswin100396@gmail.com>
2025-07-02 00:37:11 +0530

    Defined model struct which reflects to receiving JSON from SourceService

---
+++ internal/model/user.go
@@ -0,0 +1,10 @@
+package model
+
+type UserProfile struct {
+    ID            string `json:"id,omitempty"`
+    Name          string `json:"name,omitempty"`
+    Email         string `json:"email,omitempty"`
+    Mobile        string `json:"mobile,omitempty"`
+    Status        string `json:"status,omitempty"`
+    LastUpdatedAt string `json:"last_updated_at,omitempty"`
+}
